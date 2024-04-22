7ba27eb71ca4deaef019f29de4b184e36751b0d3
vir3sh <pviresh508@gmail.com>
2024-04-22 19:45:10 +0530

    task remove and  added

--- first.js
+++ first.js
@@ -13,6 +13,19 @@ function addTask(){
         let li=document.createElement("li");
         li.innerHTML=input.value;
         listContainer.appendChild(li);
-        input.value = "";
+
+        let span=document.createElement("span")
+        span.innerHTML='\u00d7'
+        li.appendChild(span)
     }
+    input.value = "";
 }
+
+listContainer.addEventListener("click",function (e){
+    if(e.target.tagName==="LI"){
+        e.target.classList.toggle('checked')
+    }
+    else if (e.target.tagName==='SPAN'){
+        e.target.parentElement.remove()
+    }
+},false)
